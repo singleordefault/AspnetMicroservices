b021990252c092a1988300b6007cedd7f9409197
Juanma <jmoliveras@kiteris.com>
2021-10-11 13:36:22 +0200

    Added basket API

---
+++ src/Services/Basket/Basket.API/Entities/ShoppingCart.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Basket.API.Entities
+{
+    public class ShoppingCart
+    {
+        public string UserName { get; set; }
+        public List<ShoppingCartItem> Items { get; set; }
+
+        public ShoppingCart()
+        {
+
+        }
+
+        public ShoppingCart(string userName)
+        {
+            UserName = userName;
+        }
+
+        public decimal TotalPrice
+        {
+            get
+            {
+                // Quiza se pueda hacer directamente con LINQ
+                decimal totalPrice = 0;
+
+                foreach (var item in Items)
+                {
+                    totalPrice += item.Price * item.Quantity;
+                }
+
+                return totalPrice;
+            }
+        }
+    }
+}
